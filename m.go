c02a8ec435ee64cf045c474695c4e18b16984d5f
aletheia7 <aletheia7@users.noreply.github.com>
2018-09-14 15:12:39 -0700

    docs

--- m.go
+++ m.go
@@ -1,5 +1,7 @@
 // Copyright 2018 aletheia7. All rights reserved. Use of this source code is
 // governed by a BSD-2-Clause license that can be found in the LICENSE file.
+
+// Provides a message bus for publish/subscribe messaging.
 package mbus
 
 import (
